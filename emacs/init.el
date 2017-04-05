06541a18d8ad587da943ed20ca62909a0b75a18f
Joel Schlosser <joel.schlosser@gtri.gatech.edu>
2017-04-05 17:38:41 -0400

    More emacs init file updates

--- emacs/init.el
+++ emacs/init.el
@@ -5,16 +5,25 @@
   ("melpa" . "http://melpa.org/packages/")
   ("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
-; Load evil mode.
-(require 'evil)
-(evil-mode 1)
-; Setup ido.
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-; Load some other plugins.
-(key-chord-mode 1)
-(global-undo-tree-mode)
+(setq use-package-always-ensure t)
+(use-package evil :config (evil-mode 1))
+  ;(evil-select-search-module 'evil-search-module 'isearch))
+(use-package evil-terminal-cursor-changer :if window-system
+  :config (evil-terminal-cursor-changer-activate))
+(use-package evil-search-highlight-persist
+  :config (global-evil-search-highlight-persist t))
+(use-package ido
+  :init (setq ido-enable-flex-matching t) (setq ido-everywhere t)
+  :config (ido-mode 1))
+(use-package undo-tree :config (global-undo-tree-mode))
+(use-package key-chord :config (key-chord-mode 1))
+(use-package ac-haskell-process
+  :config
+  (add-hook 'interactive-haskell-mode-hook 'ac-haskell-process-setup)
+  (add-hook 'haskell-interactive-mode-hook 'ac-haskell-process-setup)
+  (eval-after-load "auto-complete"
+    '(add-to-list 'ac-modes 'haskell-interactive-mode)))
+(use-package all-the-icons)
 
 ; === VISUALS ===
 ; Load theme.
@@ -40,11 +49,15 @@
 (set-face-attribute 'default nil :font my-default-font)
 (set-frame-font my-default-font nil t)
 ; Mode-based cursor box colors.
-(unless (display-graphic-p)
-  (require 'evil-terminal-cursor-changer)
-  (evil-terminal-cursor-changer-activate))
 (setq evil-normal-state-cursor '(box "orange"))
 (setq evil-insert-state-cursor '(box "yellow"))
+; Show matching parens.
+(setq show-paren-delay 0)
+(show-paren-mode 1)
+; Set matching paren color.
+(set-face-background 'show-paren-match "white")
+(set-face-foreground 'show-paren-match "black")
+(set-face-attribute 'show-paren-match nil :weight 'bold)
 
 ; === BINDINGS ===
 ; Window movement.
@@ -56,6 +69,12 @@
 (global-set-key "\M-f" 'ido-find-file)
 (global-set-key "\M-u" 'undo-tree-visualize)
 (global-set-key "\M-q" 'kill-this-buffer)
+(global-set-key "\M-Q" 'delete-window)
+(global-set-key "\M-a" 'buffer-menu)
+; Switch between header and source.
+(global-set-key (kbd "<f11>") 'ff-find-other-file)
+; Ctrl-/ to clear search results.
+(define-key evil-normal-state-map (kbd "C-/") 'evil-search-highlight-persist-remove-all)
 ; Enable Ctrl-W inside the minibuffer.
 (define-key minibuffer-local-map "\C-w" 'backward-kill-word)
 ; Enable Ctrl-U to delete to beginning of line.
@@ -65,45 +84,31 @@
   (kill-line (- 1 arg)))
 (global-set-key "\C-u" 'backward-kill-line)
 ; Spacebar scrolls cursor line to the center of the window.
-(evil-define-command evil-scroll-line-to-center (count)
-  "Scrolls line number COUNT (or the cursor line) to the center of the window."
-  :repeat nil
-  :keep-visual t
-  (interactive "P")
-  (evil-save-column
-    (let ((line (or count (line-number-at-pos (point)))))
-      (goto-char (point-min))
-      (forward-line (1- line)))
-    (recenter nil)))
 (define-key evil-normal-state-map (kbd "SPC") 'evil-scroll-line-to-center)
 ; Buffer switching with M-Tab and M-S-Tab.
 (defun my-next-buffer ()
-  "next-buffer, skip *Messages* and *Help*"
+  "next-buffer, skip *name* buffers except *scratch*"
   (interactive)
   (next-buffer)
-  (when (or (string= "*Messages*" (buffer-name))
-	    (string= "*Help*" (buffer-name)))
+  (while (and (string-prefix-p "*" (buffer-name))
+	      (not (string= "*scratch*" (buffer-name))))
       (next-buffer)))
 (defun my-previous-buffer ()
-  "previous-buffer, skip *Messages* and *Help*"
+  "previous-buffer, skip *name* buffers except *scratch*"
   (interactive)
   (previous-buffer)
-  (when (or (string= "*Messages*" (buffer-name))
-	    (string= "*Help*" (buffer-name)))
+  (while (and (string-prefix-p "*" (buffer-name))
+	      (not (string= "*scratch*" (buffer-name))))
       (previous-buffer)))
 (global-set-key [M-tab] 'my-next-buffer)
 (global-set-key (kbd "<M-S-iso-lefttab>") 'my-previous-buffer)
-
-; === FILETYPE SPECIFIC ===
-(require 'ac-haskell-process)
-(add-hook 'interactive-haskell-mode-hook 'ac-haskell-process-setup)
-(add-hook 'haskell-interactive-mode-hook 'ac-haskell-process-setup)
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'haskell-interactive-mode))
+(global-set-key (kbd "<backtab>") 'my-previous-buffer)
 
 ; === MISC ===
 ; Set middle-click to paste at cursor position instead of mouse position.
 (setq mouse-yank-at-point t)
+; Turn off line wrapping by default.
+(set-default 'truncate-lines t)
 ; Don't go past the end of the buffer for cursor movements (e.g. G in evil).
 (defun end-of-buffer-dwim (&rest args)
   (when (looking-at-p "^$")
@@ -118,7 +123,7 @@
 ; Enable autocomplete.
 (setq tab-always-indent 'complete)
 (add-to-list 'completion-styles 'initials t)
-; Don't require the final newline.
+; Don't auto-insert a final newline.
 (setq require-final-newline nil)
 ; Set some custom stuff.
 (custom-set-variables
@@ -128,6 +133,12 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ac-haskell-process haskell-mode neotree auto-complete undo-tree ido evil key-chord evil-terminal-cursor-changer evil-tabs company))))
+    (evil-search-highlight-persist all-the-icons use-package ac-haskell-process haskell-mode neotree auto-complete undo-tree ido evil key-chord evil-terminal-cursor-changer company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (message "Finished loading init.el")
--- emacs/themes/sunburst-theme.el
+++ emacs/themes/sunburst-theme.el
@@ -27,7 +27,6 @@
  ;; font-lock
  '(font-lock-builtin-face ((t (:foreground "#dd7b3b"))))
  '(font-lock-builtin-face ((t (:foreground "#dd7b3b"))))
- ;'(font-lock-comment-face ((t (:foreground "#666" ))))
  '(font-lock-comment-face ((t (:foreground "#3387cc" ))))
  '(font-lock-constant-face ((t (:foreground "#99cf50"))))
  '(font-lock-doc-string-face ((t (:foreground "#9b859d"))))
